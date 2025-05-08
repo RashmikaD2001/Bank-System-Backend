10595c6bc17e3d0322b7ca0daad5b6a025a035c5
RashmikaD2001 <rashmika.d2167@gmail.com>
2025-05-08 15:05:56 +0530

    update req file

--- machine_learning/chatbot/model/chatbot_model.py
+++ machine_learning/chatbot/model/chatbot_model.py
@@ -1,6 +1,7 @@
 import os
 from langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain_community.llms import HuggingFaceHub
 from langchain_huggingface import HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain.memory import ConversationBufferMemory
